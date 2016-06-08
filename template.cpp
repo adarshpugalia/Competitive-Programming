78b1c381b2294f7525dfc582b2c59d62d46a5485
Adarsh Pugalia <adarsh.pugaliaa@gmail.com>
2016-06-08 13:49:47 +0530

    General maths methods added.

--- template.cpp
+++ template.cpp
@@ -54,18 +54,38 @@
 #define mod(a) ((a)%MOD)
 #define bcnt __builtin_popcount 
 
+/* General maths functions. */
 ll ciel(double a) { ll ret = a; if(a-ret>EPS) ret++; return ret; }
 ll gcd(ll a, ll b) { if(a<b)return gcd(b, a); if(a%b==0)return b; return gcd(b, a%b); }
-
-/* This method does modular exponentiation in log(n). */
-ll pow(ll n, ll p) {if(p==0)return 1; if(n<=1)return n;ll res = 1;while(p){if(p&1) res = mod(res*n);n = mod(n*n);p /= 2;} return res;}
-
 double fgcd(double a, double b) { if(fabs(a)<fabs(b)) return fgcd(b, a); if(fabs(b)<GCD_EPS) return a; return fgcd(b, fmod(a,b)); }
 
 /* This method compares whether two doubles are (almost) equal. */
 bool db_db_cmp(double a, double b) { return (a+EPS>b && a-EPS<b); }
 
+/* This method generates modular factorials. */
+#define max_fact_size 100005
+ll fact[max_fact_size];
+void generate_factorials() { fact[0] = fact[1] = 1; rep(i,2,max_fact_size-1) fact[i] = mod(fact[i-1]*i); }
  
+/* This method does modular exponentiation in log(n). */
+ll mod_pow(ll n, ll p) {if(p==0)return 1; if(n<=1)return n;ll res = 1;while(p){if(p&1) res = mod(res*n);n = mod(n*n);p /= 2;} return res;}
+
+/* This method returns the modular inverse of a number (MOD must be prime). */
+ll get_inverse(ll n) { return mod_pow(n, MOD-2);}
+
+/* This method return nCr modulo MOD. */
+ll get_nCr(ll n, ll r) { return mod(ret*get_inverse(mod(fact[r]*fact[n-r]))); }
+
+/* End of maths functions. */
+
+
+/* Fast IO (does not work on codeforces) */
+int scan_d(){ int ip=getchar_unlocked(), ret=0, flag = 1; for(; ip<'0' || ip>'9'; ip=getchar_unlocked())if(ip=='-'){flag=-1; ip=getchar_unlocked(); break;}for(; ip>='0' && ip<='9'; ip=getchar_unlocked())ret=ret*10+ip-'0'; return flag*ret;}
+ll scan_ll(){int ip=getchar_unlocked(), flag = 1; ll ret = 0; for(; ip<'0'||ip>'9'; ip=getchar_unlocked())if(ip=='-'){flag=-1; ip=getchar_unlocked(); break;}for(; ip>='0' && ip<='9'; ip=getchar_unlocked())ret = ret*10+ip-'0'; return flag*ret;}
+void print_d(int n){if(n<0){n=-n; putchar_unlocked('-');}int i=10; char output_buffer[10]; do{output_buffer[--i]=(n%10)+'0'; n/=10;}while(n); do{putchar_unlocked(output_buffer[i]);}while(++i<10);}
+void print_ll(ll n){if(n<0){n=-n; putchar_unlocked('-');}int i=21; char output_buffer[21]; do{output_buffer[--i]=(n%10)+'0'; n/=10;}while(n); do{putchar_unlocked(output_buffer[i]);}while(++i<21);}
+/* End of fast IO. */ 
+
 using namespace std;
 
 void preprocess() {
