3eb53f624c3fa2d79c54fb76ea73ea867a889047
Adarsh Pugalia <adarsh.pugaliaa@gmail.com>
2016-06-18 20:38:51 +0530

    AC

---
+++ DP/Problems/Codeforces_375_B_Maximum_Submatrix_2.cpp
@@ -0,0 +1,116 @@
+/*****************************************************************************************/
+/*																		     			 */
+/*  					Adarsh Pugalia 										 			 */
+/*																			 			 */
+/*****************************************************************************************/
+ 
+#include <bits/stdc++.h>
+
+/* Data types and structures. */
+#define ll long long int
+#define llu long long int unsigned
+#define vi vector <int>
+#define vl vector <ll> 
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vpii vector <pii >
+#define vpll vector <pll >
+ 
+/* Data structure helpers. */ 
+#define f first
+#define s second
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define sz(n) (int)n.size()-1
+#define all(n) n.begin(), n.end()
+#define has(it,s) if(it!=s.end())
+ 
+/* Input output defines. */
+#define sd(n) scanf("%d", &n)
+#define sl(n) scanf("%lld", &n)
+#define slf(n) scanf("%lf", &n) 
+#define pd(n) printf("%d", n);
+#define pl(n) printf("%lld", n);
+#define plf(n) printf("%0.9lf", n);
+#define ps printf(" ")
+#define pe printf("\n")
+
+/* loops */
+#define rep(i,j,k) for(int i=j; i<=k; i++)
+#define repd(i,j,k) for(int i=j; i>=k; i--)
+#define iter(it, s) for(auto it=s.begin(); it!=s.end(); it++)
+
+/* constraints. */ 
+#define max_size 100005
+#define max_sieve_size 1000005
+#define max_matrix_size 100
+#define max_log 17
+#define INF 1000000000000000000
+#define MOD 1000000007
+#define EPS 0.0000000001
+#define GCD_EPS 0.0001
+#define PI 3.14159265358979323846
+#define mod(a) ((a)%MOD)
+#define bcnt __builtin_popcount 
+
+using namespace std;
+
+int dp[5005][5005];
+char matrix[5005][5005];
+vector<int> v;
+
+void preprocess() {
+}
+
+
+void init() {
+}
+
+
+void solve(int test_case) {
+	init();
+
+	int n, m;
+	cin>>n>>m;
+
+	rep(i,1,n) {
+		scanf("%s", matrix[i]);
+		dp[i][m+1] = 0;
+	}
+
+	repd(j,m,1) {
+		rep(i,1,n) {
+			if(matrix[i][j-1]=='1')
+				dp[i][j] = dp[i][j+1] + 1;
+			else
+				dp[i][j] = 0;
+		}
+	}
+
+	int ans = 0;
+	repd(j,m,1) {
+		v.clear();
+		rep(i,1,n) {
+			v.pb(dp[i][j]);
+		}
+		sort(all(v));
+
+		rep(i,0,sz(v)) {
+			ans = max(ans, v[i]*(n-i));
+		}
+	}
+
+	cout<<ans<<endl;
+}
+
+int main() {
+	preprocess();
+	int t = 1;
+	//sd(t);
+ 
+	rep(i,1,t) {
+		solve(i);
+	}
+	return 0;
+}
